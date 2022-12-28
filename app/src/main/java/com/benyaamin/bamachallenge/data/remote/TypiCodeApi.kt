059ed8679bf317fb93456d499967cafd7020e8c5
Benyamin Eskandari <me@benyaamin.com>
2022-12-28 23:35:48 +0330

    wrap result on Response to get if network call is successful

--- app/src/main/java/com/benyaamin/bamachallenge/data/remote/TypiCodeApi.kt
+++ app/src/main/java/com/benyaamin/bamachallenge/data/remote/TypiCodeApi.kt
@@ -2,16 +2,17 @@ package com.benyaamin.bamachallenge.data.remote
 
 import com.benyaamin.bamachallenge.data.remote.dto.Post
 import com.benyaamin.bamachallenge.data.remote.dto.User
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface TypiCodeApi {
     @GET("users")
-    suspend fun getUsers(): List<User>
+    suspend fun getUsers(): Response<List<User>>
 
     @GET("posts")
-    suspend fun getPosts(): List<Post>
+    suspend fun getPosts(): Response<List<Post>>
 
     @GET("posts/{id}")
-    suspend fun getPost(@Path("id") postId: Int): Post
+    suspend fun getPost(@Path("id") postId: Int): Response<Post>
 }
