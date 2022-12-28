342cd3c90605d08ad84d714aafd28f950cddfc7a
Benyamin Eskandari <me@benyaamin.com>
2022-12-28 23:34:24 +0330

    add insert posts

--- app/src/main/java/com/benyaamin/bamachallenge/data/local/PostsDao.kt
+++ app/src/main/java/com/benyaamin/bamachallenge/data/local/PostsDao.kt
@@ -1,13 +1,16 @@
 package com.benyaamin.bamachallenge.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.benyaamin.bamachallenge.domain.model.PostEntity
 
 @Dao
 interface PostsDao {
-
     @Query("select * from postentity")
     suspend fun getPosts(): List<PostEntity>
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertPosts(list: List<PostEntity>)
 }
