570a8c7e3dd996232b129a59645aa01146496952
Benyamin Eskandari <me@benyaamin.com>
2022-12-28 23:34:43 +0330

    add post repository interface

--- app/src/main/java/com/benyaamin/bamachallenge/domain/repository/PostRepository.kt
+++ app/src/main/java/com/benyaamin/bamachallenge/domain/repository/PostRepository.kt
@@ -1,8 +1,10 @@
 package com.benyaamin.bamachallenge.domain.repository
 
 import com.benyaamin.bamachallenge.data.remote.dto.Post
+import com.benyaamin.bamachallenge.util.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface PostRepository {
-    suspend fun getPosts(): List<Post>
-    suspend fun getPost(id: Int): Post
+    suspend fun getPosts(): Flow<Resource<List<Post>>>
+    suspend fun getPost(id: Int): Flow<Resource<Post>>
 }
