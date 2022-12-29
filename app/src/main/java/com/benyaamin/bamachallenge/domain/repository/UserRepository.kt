f68522127d9da0efa7379927fefbb706a1ef51d3
Benyamin Eskandari <me@benyaamin.com>
2022-12-29 12:00:00 +0330

    use flow

--- app/src/main/java/com/benyaamin/bamachallenge/domain/repository/UserRepository.kt
+++ app/src/main/java/com/benyaamin/bamachallenge/domain/repository/UserRepository.kt
@@ -1,7 +1,9 @@
 package com.benyaamin.bamachallenge.domain.repository
 
 import com.benyaamin.bamachallenge.data.remote.dto.User
+import com.benyaamin.bamachallenge.util.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
-    suspend fun getUsers(): List<User>
+    suspend fun getUsers(): Flow<Resource<List<User>>>
 }
