5ff3d2bb5187bacd7d93e415d9d6103b65902437
Benyamin Eskandari <me@benyaamin.com>
2022-12-29 11:59:51 +0330

    add insertUsers

--- app/src/main/java/com/benyaamin/bamachallenge/data/local/UsersDao.kt
+++ app/src/main/java/com/benyaamin/bamachallenge/data/local/UsersDao.kt
@@ -1,6 +1,8 @@
 package com.benyaamin.bamachallenge.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.benyaamin.bamachallenge.domain.model.UserEntity
 
@@ -8,4 +10,7 @@ import com.benyaamin.bamachallenge.domain.model.UserEntity
 interface UsersDao {
     @Query("select * from userentity")
     suspend fun getUsers(): List<UserEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertUsers(list: List<UserEntity>)
 }
