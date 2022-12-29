6db6a267dcb78370126ea4050e2ec01b339dc2bd
Benyamin Eskandari <me@benyaamin.com>
2022-12-29 12:29:56 +0330

    use bottomNavigation

--- app/src/main/java/com/benyaamin/bamachallenge/MainActivity.kt
+++ app/src/main/java/com/benyaamin/bamachallenge/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.benyaamin.bamachallenge
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import com.benyaamin.bamachallenge.presentation.NavGraphs
+import com.benyaamin.bamachallenge.presentation.appCurrentDestinationAsState
 import com.benyaamin.bamachallenge.ui.theme.BamaChallengeTheme
+import com.benyaamin.bamachallenge.util.BottomNavigationItems
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
+import com.ramcosta.composedestinations.navigation.navigate
+import com.ramcosta.composedestinations.utils.startDestination
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -28,6 +32,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialNavigationApi::class)
 @Composable
 fun MainScreen() {
@@ -35,12 +40,36 @@ fun MainScreen() {
     val navController = engine.rememberNavController()
 
     BamaChallengeTheme {
-        Surface(modifier = Modifier.fillMaxSize(), color = Color.LightGray) {
+        Scaffold(
+            bottomBar = {
+                BottomNavigation(navController = navController)
+            }
+        ) {
             DestinationsNavHost(
                 engine = engine,
                 navController = navController,
                 navGraph = NavGraphs.root,
                 startRoute = NavGraphs.root.startRoute
+            )
+        }
+    }
+}
+
+@Composable
+fun BottomNavigation(
+    navController: NavController
+) {
+    val currentDestination = navController.appCurrentDestinationAsState().value ?: NavGraphs.root.startDestination
+    BottomNavigation() {
+        BottomNavigationItems.values().forEach { dest ->
+            BottomNavigationItem(selected = currentDestination == dest.direction,
+                onClick = {
+                    navController.navigate(dest.direction) {
+                        launchSingleTop = true
+                    }
+                },
+                icon = { Icon(dest.icon, contentDescription = stringResource(dest.title))},
+                label = { Text(stringResource(dest.title)) },
             )
         }
     }
